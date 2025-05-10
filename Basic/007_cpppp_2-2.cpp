ca59ac905637432edac496f31af811b069910666
festla <1055317717@qq.com>
2025-05-10 22:52:16 +0800

    510exercise

---
+++ Basic/007_cpppp_2-2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double distance;
+    cout<<"Enter the distance in long: ";
+    cin>>distance;
+    cout<<"\nthe distance "<<distance<<" long";
+    cout<<" is "<<distance*220<<" yard"<<endl;
+    return 0;
+}
