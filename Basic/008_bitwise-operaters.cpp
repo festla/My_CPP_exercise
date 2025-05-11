7e2eec8dd187dac6a670f9a07133486a297b7516
festla <1055317717@qq.com>
2025-05-11 22:41:39 +0800

    增加了按位运算

--- Basic/008_bitwise-operaters.cpp
+++ Basic/008_bitwise-operaters.cpp
@@ -3,8 +3,12 @@ int main(){
     int a,b;
     std::cin >> a >> b;
     int c = a & b;
-    std::cout << c << std::endl;
+    std::cout << "按位与："<< c << std::endl;
     int d = a | b ;
-    std::cout << d << std::endl;
+    std::cout << "按位或："<< d << std::endl;
+    int e = a<<2;
+    int f = a>>2;
+    std::cout << "对a左移两位：" << e << std::endl;
+    std::cout << "对a右移两位：" << f << std::endl;
     return 0;
 }
