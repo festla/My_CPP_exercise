346236879572f8a5ccc4fd169af32d5c2344ae2d
festla <1055317717@qq.com>
2025-05-11 22:22:14 +0800

    修改 bitwise

--- Basic/008_bitwise-operaters.cpp
+++ Basic/008_bitwise-operaters.cpp
@@ -3,6 +3,8 @@ int main(){
     int a,b;
     std::cin >> a >> b;
     int c = a & b;
-    std::cout << c;
+    std::cout << c << std::endl;
+    int d = a | b ;
+    std::cout << d << std::endl;
     return 0;
 }
