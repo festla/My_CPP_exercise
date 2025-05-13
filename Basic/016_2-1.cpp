62b92837db5c34e181b49f6d58cd8826ccd44a7a
festla <1055317717@qq.com>
2025-05-13 21:59:21 +0800

    cpp primer plus 2-1

---
+++ Basic/016_2-1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name = "Qianlong Liu";
+    string adress = "Xiangtan University";
+    cout << "My name is " << name << endl;
+    cout << "My adress is " << adress << endl;
+    return 0;
+}
