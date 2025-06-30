29349259b42138c056440f1d02273bd1742cb5e2
festla <1055317717@qq.com>
2025-06-30 22:01:13 +0800

    math

--- Basic/060_energyCrystals.cpp
+++ Basic/060_energyCrystals.cpp
@@ -10,6 +10,20 @@ void solve()
     cout<<__lg(x)*2+3<<"\n";
 }
 
+/*void solve() {
+    int x;
+    cin >> x;
+    multiset<int> s = {0, 0, 0};
+    int res = 0;
+    while (*s.begin() < x) {
+        int min_val = *s.begin();
+        s.erase(s.begin());
+        int new_val = min(min_val * 2 + 1, x); // 限制增长不超过 x
+        s.insert(new_val);
+        res++;
+    }
+    cout << res << "\n";
+}*/
 int main() {
     int t;
     cin >> t;
